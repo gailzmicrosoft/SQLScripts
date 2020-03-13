4afaa6f381e3c44b3a4dd329b552547304eda7f3
gailjz <62001970+gailjz@users.noreply.github.com>
2020-03-13 18:01:07 -0400

    Add files via upload

---
+++ SQL_Server_DB_ObjCount_and_Size.sql
@@ -0,0 +1,58 @@
+
+/***************************************************************************************/
+/*           T-SQL Script to test important and relevant database statistics           */ 
+/*                          Gaiye "Gail" Zhou, Architect                               */
+/*                               March 2020                                            */
+/***************************************************************************************/
+-----------------------------------------------------------------------------------------
+-- Important: This T-Scripts will create 'TempDB..#SQL_Assessment_Info_Temp_DB'
+--            It will remove it in the end of the scritps. 
+--            Please check your DB to see if you have a table with the same name 
+-----------------------------------------------------------------------------------------
+-- This T-Scripts Produces the following important information from SQL Server for each DB
+-- (1) DbName - database name (exclude master, tempdb, msdb, model) 
+-- (2) Tables - # of Tables in this database (1)
+-- (3) Procedures - # of Stored Procedrues 
+-- (4) Views - # of Views
+-- (5) Triggers - $ of Triggers 
+-- (6) SizeMB - Size of the DB in MB
+-- (7) SizeGB - Size of the DB in GB
+-- (8) SizeTB - Size of the DB in TB 
+
+If Object_ID('TempDB..#SQL_Assessment_Info_Temp_DB','U') IS NOT NULL Drop Table #SQL_Assessment_Info_Temp_DB
+
+CREATE TABLE #SQL_Assessment_Info_Temp_DB (
+  [DbName] sysname, 
+  [Tables] int, 
+  [Procedures] int, 
+  [Views] int, 
+  [Triggers] int,
+  [SizeMB] decimal (18,2),
+  [SizeGB] decimal (18,2),
+  [SizeTB] decimal (18,2)
+  ); 
+
+DECLARE @SqlStmt NVARCHAR(MAX)
+SELECT @SqlStmt = COALESCE(@SqlStmt,'') + 'USE ' + quotename(name) + '
+Insert into #SQL_Assessment_Info_Temp_DB
+Select ' + QUOTENAME(name,'''') + ', 
+    (select count(*) from ' + QUOTENAME(Name) + '.sys.tables),
+	(select count(*) from ' + QUOTENAME(Name) + '.sys.procedures),
+    (select count(*) from ' + QUOTENAME(Name) + '.sys.views),
+    (select count(*) from ' + QUOTENAME(Name) + '.sys.triggers),
+	(select (size*8)/1024.0 from sys.master_files where name = ' + QUOTENAME(Name,'''') + ' and type_desc = ''ROWS''),
+	(select (size*8.0)/1024.0/1024.0 from sys.master_files where name = ' + QUOTENAME(Name,'''') + ' and type_desc = ''ROWS''),
+	(select (size*8.8)/1024.0/1024.0/1024.0 from sys.master_files where name = ' + QUOTENAME(Name,'''') + ' and type_desc = ''ROWS'')
+	'
+FROM sys.databases 
+where name not in ('master','tempdb', 'msdb', 'model') and state = 0 
+ORDER BY name
+
+--PRINT @SqlStmt 
+EXECUTE(@SqlStmt)
+
+Select * from #SQL_Assessment_Info_Temp_DB
+Drop Table #SQL_Assessment_Info_Temp_DB
+
+
+
