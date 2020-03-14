c9a7c39848593cd87680858a11e90b1fc685b33f
gailjz <62001970+gailjz@users.noreply.github.com>
2020-03-14 12:12:50 -0400

    Add files via upload

--- SQL_Server_DB_ObjCounts_and_Sizes.sql
+++ SQL_Server_DB_ObjCounts_and_Sizes.sql
@@ -19,7 +19,7 @@
 -- (7) SizeGB - Size of the DB in GB
 -- (8) SizeTB - Size of the DB in TB 
 
-If Object_ID('TempDB..#SQL_Assessment_Info_Temp_DB','U') IS NOT NULL Drop Table #SQL_Assessment_Info_Temp_DB
+If Object_ID('#SQL_Assessment_Info_Temp_DB','U') IS NOT NULL Drop Table #SQL_Assessment_Info_Temp_DB
 
 CREATE TABLE #SQL_Assessment_Info_Temp_DB (
   [DbName] sysname, 
